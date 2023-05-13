f47a33c06b995f442e8a24c37bd35c7e55506a44
= <=>
2023-05-14 00:06:54 +0700

    menu

--- src/app/pages/Menu/components/MenuCollection/index.tsx
+++ src/app/pages/Menu/components/MenuCollection/index.tsx
@@ -14,36 +14,36 @@ export default function MenuCollection(props) {
           <div className="sidebar_menu">
             <ul className="sidebar_menu_lv1">
               <li className="sidebar_menu_lv1-li">
-                <a className="sidebar_menu_lv1-link active ">Tất cả</a>
+                <li className="sidebar_menu_lv1-link  ">Tất cả</li>
               </li>
               {props.categories.map(category => {
                 return (
                   <>
-                    <li
-                      style={{ padding: 5, marginLeft: 30, cursor: 'pointer' }}
-                      onClick={() => {
-                        if (category.isOpen === false) category.isOpen = true;
-                        else category.isOpen = false;
-                        setChange(!change);
-                        props.setCategory(category.id);
-                      }}
-                    >
-                      {category.name}
+                    <li className="sidebar_menu_lv1-li block">
+                      <li
+                        className="sidebar_menu_lv1-link"
+                        onClick={() => {
+                          if (category.isOpen === false) category.isOpen = true;
+                          else category.isOpen = false;
+                          setChange(!change);
+                          props.setCategory(category.id);
+                        }}
+                      >
+                        {category.name}
+                      </li>
                     </li>
+
                     {category.isOpen &&
                       props.detailedCategory.map(e => {
                         if (e.categoryId === category.id)
                           return (
-                            <li
-                              style={{
-                                padding: 5,
-                                marginLeft: 40,
-                                cursor: 'pointer',
-                              }}
-                              onClick={() => props.setDetailCategoryKey(e.id)}
-                            >
-                              {e.name}
-                            </li>
+                            <ul className="sidebar_menu_lv2">
+                              <li
+                                onClick={() => props.setDetailCategoryKey(e.id)}
+                              >
+                                {e.name}
+                              </li>
+                            </ul>
                           );
                       })}
                   </>
